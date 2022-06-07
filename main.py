d5673e264982d5cc7177b085b915ffaab292a478
Mgolombane <blessingsmgolomba@gmail.com>
2022-06-07 03:04:06 -0700

    Initial commit: innitiation of files

---
+++ main.py
@@ -0,0 +1,38 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+x = "hello world"
+y = "bho bho dziko"
+
+print("english:", x)
+
+print("chichewa:", y)
+
+import matplotlib.pyplot as plt
+
+year = [1950, 1970, 1990, 2010]
+
+pop = [2.519, 3.692, 5.263, 6.972]
+
+plt.plot(year, pop)
+
+print(plt.plot(year, pop))
+
+plt.show()
+
+help(plt)
+
+#plt.scatter(gdp, life_exp, s =pop)
